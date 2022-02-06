16df6e13a7737f91215b86421169b8c308d4451c
3rmak <r3al3rmak@gmail.com>
2022-02-06 17:25:39 +0200

    #BUG_FIXED navbar works fine

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
+var toggle;
+var navTable;
+
+document.addEventListener("DOMContentLoaded", () => {
+    toggle = document.getElementById('navi-toggle');
+    navTable = document.getElementById('navigation__nav');
+
+    return navClose();
+});
+
+function navigationItemClick() {
+    return navClose();
+}
+
+function navCheckboxClick() {
+    if (toggle.checked) {
+        return navOpen();
+    }
+
+    return navClose();
+}
+
 function navClose() {
-    document.getElementById('navi-toggle').checked = false;
+    setTimeout(() => {
+        toggle.checked = false;
+        navTable.style.display = 'none';
+    }, 200)  
+}
+
+function navOpen() {
+    setTimeout(() => {
+        toggle.checked = true;
+        navTable.style.display = 'block';
+    }, 360)
+    
 }
